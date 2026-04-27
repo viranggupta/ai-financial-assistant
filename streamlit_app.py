515ea17385a50e7477ce7126edf69019f6a9bb53
viranggupta <viranggupta@gmail.com>
2026-04-28 00:19:01 +0200

    removed hardcoded token

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
 import os
+load_dotenv()
+import time
+
 
 # Load API
 client = InferenceClient(
-    model="google/flan-t5-base",
+    model="tiiuae/falcon-rw-1b",
     token=os.environ["HF_TOKEN"]
 )
+import time  
 
 def generate_response(user_query):
     prompt = f"""
@@ -16,12 +21,14 @@ Answer clearly and professionally:
 {user_query}
 """
 
-    try:
-        response = client.text_generation(prompt, max_new_tokens=100)
-        return response
-    except Exception as e:
-        return "⚠️ Server busy. Please try again."
+    for _ in range(3):  # retry 3 times
+        try:
+            response = client.text_generation(prompt, max_new_tokens=100)
+            return response
+        except Exception:
+            time.sleep(2)
 
+    return "⚠️ Server busy. Please try again in a few seconds."
 # UI
 st.set_page_config(page_title="AI Financial Assistant")
 st.title("💰 AI Financial Assistant")
